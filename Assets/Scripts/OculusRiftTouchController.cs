22591318ec03411d12b5453277fa3fc7ade79638
Weidong Guo <weidongguo@live.com>
2017-11-16 18:05:06 -0800

    read subscribed msg, and it's in the form of JSON string

--- Assets/Scripts/OculusRiftTouchController.cs
+++ Assets/Scripts/OculusRiftTouchController.cs
@@ -9,7 +9,6 @@ using System.Runtime.InteropServices;
 
 using System.Text;
 
-
 public class OculusRiftTouchController : MonoBehaviour {
     [DllImport("ROSClient.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
     public static extern IntPtr _ROSClient_init();
@@ -27,7 +26,7 @@ public class OculusRiftTouchController : MonoBehaviour {
     public static extern bool _ROSClient_isMsgAvailable(IntPtr client);
 
     [DllImport("ROSClient.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
-    public static extern StringBuilder _ROSClient_getMsg(IntPtr client);
+    public static extern IntPtr _ROSClient_getMsg(IntPtr client);
 
     [DllImport("ROSClient.dll", CharSet = CharSet.Unicode, CallingConvention = CallingConvention.Cdecl)]
     public static extern void _ROSClient_clearMsg(IntPtr client);
@@ -35,11 +34,12 @@ public class OculusRiftTouchController : MonoBehaviour {
     IntPtr ROSClient;
 	int[] buttons;
 	float[] axes;
-	StringBuilder str;
+    string msg;	
 	// Use this for initialization
 	void Start () {
 		ROSClient = _ROSClient_init();
 		_ROSClient_initPublisher(ROSClient);//, new StringBuilder("hello"));
+		_ROSClient_initSubscriber(ROSClient);
 		buttons = new int[11];
 		axes = new float[8];
 	}
@@ -47,19 +47,16 @@ public class OculusRiftTouchController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		OVRInput.Update(); // Has to be called at the beginning to interact with OVRInput.
-
+        
+        // Retrieve buttons status
 		buttons[0] = OVRInput.Get(OVRInput.Button.One) ? 1 : 0;
 		buttons[1] = OVRInput.Get(OVRInput.Button.Two) ? 1 : 0;
 		buttons[2] = OVRInput.Get(OVRInput.Button.Three) ? 1 : 0;
 		buttons[3] = OVRInput.Get(OVRInput.Button.Four) ? 1 : 0;
 		buttons[9] = OVRInput.Get(OVRInput.Button.PrimaryThumbstick) ? 1 : 0;
 		buttons[10] = OVRInput.Get(OVRInput.Button.SecondaryThumbstick) ? 1 : 0;
-		/*
-		for (int i = 0; i < buttons.Length; i++)
-		{
-		    Debug.Log(i + ": "  + buttons[i]);
-		}	
-		*/
+        
+        // Retrieve axes status
 		Vector2 axisStickLeft = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
 		axes[0] = axisStickLeft.x;
 		axes[1] = axisStickLeft.y;
@@ -71,19 +68,16 @@ public class OculusRiftTouchController : MonoBehaviour {
 		axes[4] = axisStickRight.y;
 		
 		axes[5] = OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger);
-		/*
-		for (int i = 0; i < axes.Length; i++)
-		{
-		    Debug.Log(i + ": "  + axes[i]);
-		}	
-		*/
+        
+        // Send buttons and axes to ROS.	
 		_ROSClient_publish(ROSClient, buttons, 11, axes, 8);
-	
+        
+        // Read position from ROS.	
 		if (_ROSClient_isMsgAvailable(ROSClient))
 		{
-		    str = _ROSClient_getMsg(ROSClient);
+		    msg = Marshal.PtrToStringAnsi(_ROSClient_getMsg(ROSClient));
 		    _ROSClient_clearMsg(ROSClient);
-		    Debug.Log(str); 
+		    Debug.Log(msg); 
 		}
     }
 }
