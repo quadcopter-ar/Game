b3b93778fe861f1a93b3d737254415dae3c68b29
Siyu <siyujiang96@gmail.com>
2019-04-29 11:50:11 -0700

    Shooting with player collision and wall collision detection

---
+++ shooting/Assets/shooting/scripts/Bullets.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullets : MonoBehaviour 
+{
+
+    void OnCollisionEnter(Collision collision)
+    {
+    	var target = collision.gameObject;
+		var health = target.GetComponent<Health>();
+		if (health != null)
+		{
+		    health.TakeDamage(10);
+		}
+
+        Destroy(gameObject);
+
+    }
+}
